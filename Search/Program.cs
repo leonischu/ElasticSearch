1ae3de1cb3a03a730f1c5b9d62cc941d869a92ce
leonischu <nischal.201423@ncit.edu.np>
2026-04-21 15:20:44 +0545

    completed  elastic search

--- Search/Program.cs
+++ Search/Program.cs
@@ -1,10 +1,31 @@
+using Nest;
+using Search.Models;
+using Search.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+
+var setting = new ConnectionSettings(new Uri("http://localhost:9200/")).DefaultIndex("esdemo");
+var client = new ElasticClient(setting);
+
+builder.Services.AddSingleton(client);
+
+builder.Services.AddScoped<IElasticSearchService<MyDocument>, ElasticSearchService<MyDocument>>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
