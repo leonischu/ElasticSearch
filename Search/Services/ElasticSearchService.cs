658fc06c7b5d500ab5924c6386da3e0395ed49fa
leonischu <nischal.201423@ncit.edu.np>
2026-04-21 14:34:04 +0545

    First Commit

---
+++ Search/Services/ElasticSearchService.cs
@@ -0,0 +1,45 @@
+﻿
+using Nest;
+
+namespace Search.Services
+{
+    public class ElasticSearchService<T> : IElasticSearchService<T> where T : class
+    {
+
+        private readonly ElasticClient _elasticClient;
+
+        public ElasticSearchService(ElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+
+
+        public async Task<string> CreateDocumentAsync(T document)
+        {
+           var response =  await _elasticClient.IndexDocumentAsync(document);
+            return response.IsValid ? "Document created sucessfully" : "Failed to create document";
+        }
+
+        public async  Task<string> DeleteDocumentAsync(int id)
+        {
+            var response = await _elasticClient.DeleteAsync(new DocumentPath<T>(id));
+            return response.IsValid ? "Document deleted sucessfully" : "Failed to delete document";
+        }
+
+        public Task<IEnumerable<T>> GetAllDocuments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetDocumentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateDocumentAsync(T document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Search/Services/IElasticSearchService.cs
@@ -0,0 +1,15 @@
+﻿namespace Search.Services
+{
+    public interface IElasticSearchService<T>
+    {
+        Task<string> CreateDocumentAsync(T document);
+
+        Task<T> GetDocumentAsync(int id);
+
+        Task<IEnumerable<T>> GetAllDocuments();
+        
+        Task<string> UpdateDocumentAsync(T document);
+
+        Task<string> DeleteDocumentAsync(int id);
+    }
+}
